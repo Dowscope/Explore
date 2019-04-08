58f3ea2b2f643eb6af9f5a46589e7debf75739bc
Tim Dowling <timothy.dowling@me.com>
2019-04-07 21:34:14 -0400

    Inital Commit
    Currently Working on the terrain

---
+++ Explore/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
