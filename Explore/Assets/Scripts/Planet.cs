102660c82b5dfa219524c93d7cb3b81db8c7ab33
Tim Dowling <timothy.dowling@me.com>
2019-05-17 18:58:44 -0400

    Changed to planet generation
    Switched to 3d and starting now from a planet generation

---
+++ Explore/Assets/Scripts/Planet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [Range(2, 256)]
+    public int resolution = 10;
+
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrianFace[] terrianFaces;
+
+    private void OnValidate()
+    {
+        initialize();
+        GenerateMesh();
+    }
+    void initialize()
+    {
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+
+        terrianFaces = new TerrianFace[6];
+
+        Vector3[] direction = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObject = new GameObject("Mesh");
+                meshObject.transform.parent = transform;
+
+                meshObject.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObject.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+
+            }
+
+            terrianFaces[i] = new TerrianFace(meshFilters[i].sharedMesh, resolution, direction[i]);
+        }
+    }
+    void GenerateMesh()
+    {
+        foreach (TerrianFace face in terrianFaces)
+        {
+            face.constructMesh();
+        }
+    }
+}
