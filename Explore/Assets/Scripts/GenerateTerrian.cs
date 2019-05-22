2959a41b5cc487a060c0e31267f237c07609cb16
Tim Dowling <timothy.dowling@me.com>
2019-05-22 17:28:37 -0400

    Made a switch to have just an on land game.

---
+++ Explore/Assets/Scripts/GenerateTerrian.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrian : MonoBehaviour
+{
+    Mesh mesh;
+
+    int heightScale = 5;
+    float detailScale = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x)/detailScale,
+                                              (vertices[i].z + this.transform.position.z)/detailScale)*heightScale;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        this.gameObject.AddComponent<MeshCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Explore/Assets/Scripts/WorldController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    public GameObject terrianChunk;
+
+    Hashtable chunks = new Hashtable();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = Instantiate(terrianChunk);
+        go.name = "Chunk_" + go.transform.position.x + "_" + go.transform.position.z;
+        go.transform.parent = this.transform;
+        chunks.Add(go.name, go);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
